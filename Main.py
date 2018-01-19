0cc2c12ddd20175c0ddc407186fab75683f9e7f9
Amin Karimi Monsefi <BackendMi@hotmail.com>
2018-01-19 18:40:25 +0330

    asd

---
+++ Main.py
@@ -0,0 +1,49 @@
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from PIL import Image
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+# m_train = train_x_orig.shape[0]
+# num_px = train_x_orig.shape[1]
+# m_test = test_x_orig.shape[0]
+
+np.random.seed(1)
+
+n_x = 12288     # num_px * num_px * 3
+n_h = 7
+n_y = 1
+layers_dims = (n_x, n_h, n_y)
+
+
+def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
+
+    np.random.seed(1)
+    costs = []                         # keep track of cost
+
+    parameters = None
+
+    for i in range(0, num_iterations):
+
+        cost = None
+
+        grads = None
+
+        parameters = None
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
